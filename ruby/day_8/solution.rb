430dee0478d87e87fcc1bebb7b8ebe65e33adfd0
eddbot <edwardsansome@gmail.com>
2023-12-08 14:00:39 +0000

    little refactor

--- ruby/day_8/solution.rb
+++ ruby/day_8/solution.rb
@@ -1,54 +1,128 @@
 # frozen_string_literal: true
 
-input = File.read('./inputs/input.txt').split("\n\n")
-
-instructions = input.first
-
-Node = Struct.new(:val, :left, :right, :rawL, :rawR)
-
-raw_nodes = input.last.split("\n").each_with_object({}) do |node, hsh|
-  n = node.gsub(' ', '').split(/\W/)
-  hsh[n[0]] = Node.new(n[0], nil, nil, n[2], n[3])
-end
-
-raw_nodes.each_value do |v|
-  v.left = raw_nodes[v.rawL]
-  v.right = raw_nodes[v.rawR]
-end
-
-starting_node = 'AAA'
-
-# part 1
-counter = 0
-instructions.split('').cycle do |i|
-  break if starting_node == 'ZZZ'
-
-  starting_node = raw_nodes[starting_node].left.val if i == 'L'
-  starting_node = raw_nodes[starting_node].right.val if i == 'R'
-  counter += 1
-end
-
-puts "part 1 = #{counter}"
-
-# part 2
-starting_nodes = raw_nodes.keys.filter { |k| k.end_with?('A') }
-starting_node.size
-
-routes = []
-
-starting_nodes.each do |starting_node|
-  counter = 0
-  instructions.split('').cycle do |i|
-    break if starting_node.end_with?('Z')
-
-    starting_node = raw_nodes[starting_node].left.val if i == 'L'
-    starting_node = raw_nodes[starting_node].right.val if i == 'R'
-
-    counter += 1
+class NodeTree
+  def initialize(input)
+    @input = input
+    @node_map = {}
   end
 
-  routes << counter
-  counter = 0
+  def self.create(...)
+    new(...).create
+  end
+
+  def create
+    construct_initial_node_tree
+    link_nodes
+  end
+
+  private
+
+  Node = Struct.new(:val, :left, :right)
+
+  def construct_initial_node_tree
+    input_lines.each do |raw_node|
+      node = node_creator(raw_node)
+      node_map[node.val] = node
+    end
+  end
+
+  def link_nodes
+    node_map.each_value do |v|
+      v.left = node_map[v.left]
+      v.right = node_map[v.right]
+    end
+  end
+
+  def node_creator(raw_node)
+    node_values = raw_node.gsub(' ', '').split(/\W/)
+
+    Node.new(node_values[0], node_values[2], node_values[3])
+  end
+
+  def input_lines
+    input.last.split("\n")
+  end
+
+  attr_reader :input, :node_map
 end
 
-puts "part 2 = #{routes.reduce(1) { |acc, n| acc.lcm(n) }}"
+class Solution
+  def initialize(nodes, instructions)
+    @nodes = nodes
+    @instructions = instructions
+  end
+
+  private
+
+  def move(node, instruction)
+    return nodes[node].left.val if instruction == 'L'
+
+    nodes[node].right.val
+  end
+
+  attr_reader :nodes, :instructions
+end
+
+class PartOne < Solution
+  def solve
+    starting_node = 'AAA'
+    counter = 0
+    instructions.split('').cycle do |i|
+      break if solved?(starting_node)
+
+      starting_node = move(starting_node, i)
+
+      counter += 1
+    end
+    counter
+  end
+
+  private
+
+  def solved?(node)
+    node == 'ZZZ'
+  end
+end
+
+class PartTwo < Solution
+  def solve
+    calc.reduce(1) { |acc, n| acc.lcm(n) }
+  end
+
+  private
+
+  def calc
+    starting_nodes.each_with_object([]) do |starting_node, routes|
+      counter = 0
+      instructions.split('').cycle do |i|
+        break if solved?(starting_node)
+
+        starting_node = move(starting_node, i)
+
+        counter += 1
+      end
+
+      routes << counter
+    end
+  end
+
+  def starting_nodes
+    nodes.keys.filter { |k| k.end_with?('A') }
+  end
+
+  def solved?(node)
+    node.end_with?('Z')
+  end
+end
+
+input = File.read('./inputs/input.txt').split("\n\n")
+instructions = input.first
+raw_nodes = NodeTree.create(input)
+part_one = PartOne.new(raw_nodes, instructions)
+part_two = PartTwo.new(raw_nodes, instructions)
+
+# part 1
+puts "part 1 = #{part_one.solve}"
+
+# part 2
+puts "part 2 = #{part_two.solve}"
