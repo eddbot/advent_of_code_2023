44311cb2c9e56303f87a334b60a6d1caae16288b
eddbot <edwardsansome@gmail.com>
2023-12-17 19:29:02 +0000

    day_16: part two

--- src/main/java/com/murismo/solution/day_16/Contraption.java
+++ src/main/java/com/murismo/solution/day_16/Contraption.java
@@ -1,6 +1,5 @@
 package com.murismo.solution.day_16;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Contraption {
@@ -22,28 +21,28 @@ public class Contraption {
 
         for (int i = 0; i < HEIGHT; i++) {
             var line = input.get(i).toCharArray();
-            for( int j = 0; j < WIDTH; j++){
-               grid[i][j] = line[j];
-               visited[i][j] = "";
+            for (int j = 0; j < WIDTH; j++) {
+                grid[i][j] = line[j];
+                visited[i][j] = "";
             }
         }
         this.grid = grid;
         this.visited = visited;
     }
 
-    private void resetVisited(){
+    private void resetVisited() {
         for (int i = 0; i < HEIGHT; i++) {
-            for( int j = 0; j < WIDTH; j++){
+            for (int j = 0; j < WIDTH; j++) {
                 this.visited[i][j] = "";
             }
         }
     }
 
-    private int energised(){
+    private int energised() {
         var counter = 0;
         for (String[] lines : visited) {
             for (String line : lines) {
-                if(!line.isEmpty()){
+                if (!line.isEmpty()) {
                     counter++;
                 }
             }
@@ -54,84 +53,71 @@ public class Contraption {
     }
 
 
-    public void partTwo(){
+    public void partTwo() {
 
         int best = 0;
-        int counter;
 
-        // down
-        for (int i = 0; i <= WIDTH; i++) {
+        for (int i = 0; i < HEIGHT; i++) {
+            beam(i, 0, 'R');
+            var counter = energised();
+            if (counter > best) {
+                best = counter;
+            }
+
+            beam(i, WIDTH - 1, 'L');
+            counter = energised();
+            if (counter > best) {
+                best = counter;
+            }
+        }
+
+        for (int i = 0; i < WIDTH; i++) {
             beam(0, i, 'D');
+            var counter = energised();
+            if (counter > best) {
+                best = counter;
+            }
+
+            beam(HEIGHT - 1, i, 'U');
             counter = energised();
-            if(counter > best) {
+            if (counter > best) {
                 best = counter;
             }
         }
 
-//         up
-        for (int i = 0; i <= WIDTH; i++) {
-            beam(HEIGHT, i, 'U');
-            counter = energised();
-            if(counter > best) {
-                best = counter;
-            }
-        }
 
-//         right
-        for (int i = 0; i <= HEIGHT; i++) {
-            beam(i, 0 , 'R');
-            counter = energised();
-            if(counter > best) {
-                best = counter;
-            }
-        }
-
-        // left
-
-        for (int i = 0; i <= HEIGHT; i++) {
-            beam(i, WIDTH , 'L');
-            counter = energised();
-            if(counter > best) {
-                best = counter;
-            }
-        }
-//
         System.out.println(best);
     }
 
-    public int on() {
+    public void on() {
 
         // we start at 0,0 and go right
-        beam(0,0, 'R');
+        beam(0, 0, 'R');
 
         var counter = 0;
 
         for (String[] lines : visited) {
             for (String line : lines) {
-                if(!line.isEmpty()){
+                if (!line.isEmpty()) {
                     counter++;
                 }
             }
         }
+        resetVisited();
 
         System.out.println(counter);
-
-        return -1;
     }
 
-    private void beam(int i, int j, char direction){
-
-
-
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid.length){
+    private void beam(int i, int j, char direction) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid.length) {
             return;
         }
 
-        if(visited[i][j].contains(Character.toString(direction))) {
+        if (visited[i][j].contains(Character.toString(direction))) {
             return;
         }
 
-        var caret = switch (direction){
+        var caret = switch (direction) {
             case 'R' -> '>';
             case 'L' -> '<';
             case 'D' -> 'v';
@@ -157,7 +143,7 @@ public class Contraption {
 //        }
 
 
-        switch (square){
+        switch (square) {
             case '.' -> {
                 switch (direction) {
                     case 'R' -> beam(i, j + 1, direction);
@@ -167,39 +153,39 @@ public class Contraption {
                 }
             }
             case '|' -> {
-                switch (direction){
+                switch (direction) {
                     case 'R', 'L' -> {
-                        beam(i-1, j, 'U');
-                        beam(i+1, j, 'D');
+                        beam(i - 1, j, 'U');
+                        beam(i + 1, j, 'D');
                     }
-                    case 'U' -> beam(i-1, j, direction);
-                    case 'D' -> beam(i+1, j, direction);
+                    case 'U' -> beam(i - 1, j, direction);
+                    case 'D' -> beam(i + 1, j, direction);
                 }
             }
             case '-' -> {
-                switch (direction){
+                switch (direction) {
                     case 'U', 'D' -> {
-                        beam(i, j+1, 'R');
-                        beam(i, j-1, 'L');
+                        beam(i, j + 1, 'R');
+                        beam(i, j - 1, 'L');
                     }
-                    case 'R' -> beam(i, j+1, direction);
-                    case 'L' -> beam(i, j-1, direction);
+                    case 'R' -> beam(i, j + 1, direction);
+                    case 'L' -> beam(i, j - 1, direction);
                 }
             }
             case '/' -> {
-                switch (direction){
-                    case 'U' -> beam(i, j+1, 'R');
-                    case 'D' -> beam(i, j-1, 'L');
-                    case 'R' -> beam(i-1, j, 'U');
-                    case 'L' -> beam(i+1, j, 'D');
+                switch (direction) {
+                    case 'U' -> beam(i, j + 1, 'R');
+                    case 'D' -> beam(i, j - 1, 'L');
+                    case 'R' -> beam(i - 1, j, 'U');
+                    case 'L' -> beam(i + 1, j, 'D');
                 }
             }
             case '\\' -> {
-                switch (direction){
-                    case 'U' -> beam(i, j-1, 'L');
-                    case 'D' -> beam(i, j+1, 'R');
-                    case 'R' -> beam(i+1, j, 'D');
-                    case 'L' -> beam(i-1, j, 'U');
+                switch (direction) {
+                    case 'U' -> beam(i, j - 1, 'L');
+                    case 'D' -> beam(i, j + 1, 'R');
+                    case 'R' -> beam(i + 1, j, 'D');
+                    case 'L' -> beam(i - 1, j, 'U');
                 }
             }
             default -> throw new RuntimeException(Character.toString(square));
